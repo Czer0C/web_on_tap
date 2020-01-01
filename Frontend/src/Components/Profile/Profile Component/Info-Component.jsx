fc2459329b07b7393e7ec6bd47b415cb7ccebf7e
Czer0C <trieunguyen04239@gmail.com>
2020-01-01 10:51:33 +0700

    khởi tạo cập nhật thông tin

--- Frontend/src/Components/Profile/Profile Component/Info-Component.jsx
+++ Frontend/src/Components/Profile/Profile Component/Info-Component.jsx
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
 import './Info.css';
+import Modal from 'react-responsive-modal'; 
 
 export default class InfoComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
-        } = this.props
+            inputUsername: this.props.info.TenDangNhap,
+            inputEmail: this.props.info.Email,
+            inputFullname: this.props.info.HoTen,
+            inputGrade: this.props.info.Lop
+        }
+        this.handleChangeInputUsername = this.handleChangeInputUsername.bind(this)
+        this.handleChangeInputEmail = this.handleChangeInputEmail.bind(this)
+        this.handleChangeInputFullname = this.handleChangeInputFullname.bind(this)
+        this.handleChangeInputGrade = this.handleChangeInputGrade.bind(this)
+        this.handleUpdate = this.handleUpdate.bind(this)
     }
+    handleChangeInputUsername(event){this.setState({inputUsername:event.target.value})}
+    handleChangeInputEmail(event){this.setState({inputEmail:event.target.value})}
+    handleChangeInputFullname(event){this.setState({inputFullname:event.target.value})}
+    handleChangeInputGrade(event){this.setState({inputGrade:event.target.value})}
+
+
 
     componentDidMount() {
         
     }
 
+    handleUpdate(event) {
+        event.preventDefault()
+        
+    }
     renderUserType(type) {
         switch (type) {
             case 1: 
@@ -26,74 +45,91 @@ export default class InfoComponent extends Component {
 
     render() {
         const {
-            info
+            inputUsername,
+            inputEmail,
+            inputFullname,
+            inputGrade
         } = this.state
         return (
             <div>
                 {   
-                    !info ? 
+                    !inputFullname ? 
                     <img id="loading" src="https://i.imgur.com/FMpRIoS.gif"></img>  : 
                     <form>
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label className="profile-info-label" for="inputUsername">Tên Đăng Nhập</label>
-                                <input type="text" class="form-control profile-input" id="inputUsername" value={info.TenDangNhap} placeholder="Tên Đăng Nhập"/>
+                                <input 
+                                    type="text" 
+                                    class="form-control profile-input" 
+                                    id="inputUsername" 
+                                    value={inputUsername} 
+                                    placeholder="Tên Đăng Nhập"
+                                    onChange={this.handleChangeInputUsername}
+                                />
                             </div>
                             <div class="form-group col-md-6">
                                 <label className="profile-info-label" for="inputEmail4">Email</label>
-                                <input type="email" class="form-control profile-input" id="inputEmail4" value={info.Email} placeholder="Email"/>
+                                <input 
+                                    type="email" 
+                                    class="form-control profile-input" 
+                                    id="inputEmail4" 
+                                    value={inputEmail} 
+                                    placeholder="Email"
+                                    onChange={this.handleChangeInputEmail}
+                                />
                             </div>
                         </div>
                         <div className="form-row">
                             <div class="form-group col-md-6">
                                 <label className="profile-info-label" for="inputFullname">Họ Tên</label>
-                                <input type="text" class="form-control profile-input" id="inputFullname" value={info.HoTen} placeholder="Họ Tên"/>
+                                <input 
+                                    type="text" 
+                                    class="form-control profile-input" 
+                                    id="inputFullname" 
+                                    value={inputFullname} 
+                                    placeholder="Họ Tên"
+                                    onChange={this.handleChangeInputFullname}
+                                />
                             </div>
                             <div class="form-group col-md-6">
                                 <label className="profile-info-label" for="inputGrade">Lớp</label>
-                                <select id="inputGrade" class="form-control profile-input">
-                                    <option selected>{info.Lop}</option>
-                                    <option>...</option>
+                                <select 
+                                    id="inputGrade" 
+                                    class="form-control profile-input" 
+                                    onChange={this.handleChangeInputGrade}
+                                >
+                                    <option selected={inputGrade === 1} value="1">1</option>
+                                    <option selected={inputGrade === 2} value="2">2</option>
+                                    <option selected={inputGrade === 3} value="3">3</option>
+                                    <option selected={inputGrade === 4} value="4">4</option>                                    
+                                    <option selected={inputGrade === 5} value="5">5</option>
                                 </select>
                             </div>
                         </div>
                         <div class="form-row">    
                             <div class="form-group col-md-6 ml-auto">
                                 <label className="profile-info-label" for="inputUserType">Loại Người Dùng</label>
-                                <input type="text" class="form-control" id="inputUserType" value={this.renderUserType(info.LoaiNguoiDung)} disabled/>
+                                <input type="text" class="form-control" id="inputUserType" value={this.renderUserType(this.props.info.LoaiNguoiDung)} disabled/>
                             </div>
                             <div class="form-group col-md-6 ml-auto">
                                 <label className="profile-info-label" for="inputEXP">Điểm Tích Lũy</label>
-                                <input type="text" class="form-control" id="inputEXP" value={info.DiemTichLuy} disabled/>
+                                <input type="text" class="form-control" id="inputEXP" value={this.props.info.DiemTichLuy} disabled/>
                             </div>
                         </div>
 
 
 
-                        <button type="submit" class="btn btn-info">Cập Nhật</button>
+                        <button 
+                            type="submit" 
+                            class="btn btn-info" 
+                            onClick={this.handleUpdate}
+                        >
+                            Cập Nhật
+                        </button>
                     </form>
                 }
             </div>
         )
     }
 }
-
-
-{/*
-
-<div class="form-row">
-                            <div class="form-group col-md-4">
-                                <label className="profile-info-label" for="inputPassword">Mật Khẩu Cũ</label>
-                                <input type="password" class="form-control" id="inputPassword" value={""} placeholder="Mật Khẩu Cũ"/>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label className="profile-info-label" for="inputNewPassword">Mật Khẩu Mới</label>
-                                <input type="password" class="form-control" id="inputNewPassword" value={""} placeholder="Mật Khẩu Mới"/>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label className="profile-info-label" for="inputPasswordRe">Nhập lại Mật Khẩu mới</label>
-                                <input type="password" class="form-control" id="inputPasswordRe" value={""} placeholder="Mật Khẩu Mới"/>
-                            </div>
-                        </div>
-
-*/}
